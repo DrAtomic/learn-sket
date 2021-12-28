44497f267b4d728b8de1f1eeefc93488ec3569dd
DrAtomic <leo.ward.garcia@gmail.com>
2021-12-27 21:06:26 -0800

    static static static!

--- cl-art.lisp
+++ cl-art.lisp
@@ -2,20 +2,24 @@
 
 (in-package #:cl-art)
 
+
 (defun make-grid (width height number-of-rect)
   (let ((width-space (/ width number-of-rect))
 	(height-space (/ height number-of-rect)))
     (dotimes (x number-of-rect)
       (dotimes (y number-of-rect)
-	(rect (* x width-space) (* y height-space) 100 100)))))
+	(with-pen (make-pen :fill (color-filter-grayscale(hash-color (random 255))))
+	  (rect (* x width-space)
+		(* y height-space)
+		height-space
+		height-space))))))
 
 (defsketch art
     ((title "simple rects")
      (width 800)
-     (height 600)
+     (height 800)
      (copy-pixels t))
-  (background (gray 1))
-  (make-grid width height 5))
+  (make-grid width height 200))
 
 (defmethod setup ((instance art) &key &allow-other-keys)
   (background (gray 1)))
