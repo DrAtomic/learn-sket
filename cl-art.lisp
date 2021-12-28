c86bf79e385b375e2053b0e8e08d18c0b1489a59
DrAtomic <leo.ward.garcia@gmail.com>
2021-12-27 20:42:20 -0800

    made a grid of boxes

--- cl-art.lisp
+++ cl-art.lisp
@@ -2,13 +2,20 @@
 
 (in-package #:cl-art)
 
+(defun make-grid (width height number-of-rect)
+  (let ((width-space (/ width number-of-rect))
+	(height-space (/ height number-of-rect)))
+    (dotimes (x number-of-rect)
+      (dotimes (y number-of-rect)
+	(rect (* x width-space) (* y height-space) 100 100)))))
+
 (defsketch art
     ((title "simple rects")
      (width 800)
      (height 600)
      (copy-pixels t))
-  (rect 300 10 100 100)
-  (rect 100 550 100 100))
+  (background (gray 1))
+  (make-grid width height 5))
 
 (defmethod setup ((instance art) &key &allow-other-keys)
   (background (gray 1)))
