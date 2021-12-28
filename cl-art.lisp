3801b6cdb829e0681cbc0586d94d7ead1e3ccd86
DrAtomic <leo.ward.garcia@gmail.com>
2021-12-27 22:07:10 -0800

    push and pop matrix

--- cl-art.lisp
+++ cl-art.lisp
@@ -3,23 +3,23 @@
 (in-package #:cl-art)
 
 
-(defun make-grid (width height number-of-rect)
-  (let ((width-space (/ width number-of-rect))
-	(height-space (/ height number-of-rect)))
-    (dotimes (x number-of-rect)
-      (dotimes (y number-of-rect)
-	(with-pen (make-pen :fill (color-filter-grayscale(hash-color (random 255))))
-	  (rect (* x width-space)
-		(* y height-space)
-		height-space
-		height-space))))))
+(defun make-grid (width height number-of-elm)
+  (let ((width-space (/ width number-of-elm))
+	(height-space (/ height number-of-elm)))
+    (dotimes (x number-of-elm)
+      (dotimes (y number-of-elm)
+	(with-pen (make-pen :stroke +black+ :weight 1))
+	(push-matrix)
+	(translate (* x width-space) (* y height-space))
+	(line 0 0 width-space 0)
+	(pop-matrix)))))
 
 (defsketch art
     ((title "simple rects")
      (width 800)
-     (height 800)
-     (copy-pixels t))
-  (make-grid width height 200))
+     (height 800))
+  (background (gray 1))
+  (make-grid width height 10))
 
 (defmethod setup ((instance art) &key &allow-other-keys)
   (background (gray 1)))
