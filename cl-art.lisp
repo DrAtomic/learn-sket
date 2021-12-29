61a929f6ca1d80b96e30e4da36b74aca265fb3ec
DrAtomic <leo.ward.garcia@gmail.com>
2021-12-29 09:50:57 -0800

    makes a bunch of spinny vectors

--- cl-art.lisp
+++ cl-art.lisp
@@ -2,26 +2,35 @@
 
 (in-package #:cl-art)
 
+(defun angle (x1 y1 x2 y2)
+  "gets the angle from a line"
+  (let ((a (atan (- y2 y1) (- x2 x1))))
+    (if (< a 0)
+	(+ a (* PI 2.0))
+	a)))
 
 (defun make-grid (width height number-of-elm)
+  
   (let ((width-space (/ width number-of-elm))
 	(height-space (/ height number-of-elm)))
     (dotimes (x number-of-elm)
       (dotimes (y number-of-elm)
-	(with-pen (make-pen :stroke +black+ :weight 1))
-	(push-matrix)
-	(translate (* x width-space) (* y height-space))
-	(line 0 0 width-space 0)
-	(pop-matrix)))))
+	(with-pen (make-pen :stroke +black+ :weight 1)
+	  (push-matrix)
+	  (translate (* x width-space) (* y height-space))
+	  (rotate (random 360))
+	  (line 0 0 width-space 0)
+	  (pop-matrix))))))
 
 (defsketch art
     ((title "simple rects")
      (width 800)
      (height 800))
   (background (gray 1))
-  (make-grid width height 10))
+  (make-grid 800 800 20))
 
 (defmethod setup ((instance art) &key &allow-other-keys)
-  (background (gray 1)))
+  (background (gray 1))
+)
 
 (make-instance 'cl-art:art)
